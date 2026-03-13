0dbc025fbc59dbe119e56eef8530239d5b82a43c
Yash_Vardhan_Rajpoot <yashvardhanrajpoot15@gmail.com>
2026-03-14 02:51:50 +0530

    Refactor compute_similarity to use p=2 and torch.mm

--- Multimodal Misinformation Classifier/Utils/similarity.py
+++ Multimodal Misinformation Classifier/Utils/similarity.py
@@ -3,9 +3,9 @@ import torch.nn.functional as F
 
 def compute_similarity(image_embedding, text_embedding):
 
-    image_embedding = F.normalize(image_embedding, dim=-1)
-    text_embedding = F.normalize(text_embedding, dim=-1)
+    image_embedding = F.normalize(image_embedding, p=2, dim=1)
+    text_embedding = F.normalize(text_embedding, p=2, dim=1)
 
-    similarity = torch.matmul(image_embedding, text_embedding.T)
+    similarity = torch.mm(image_embedding, text_embedding.T)
 
     return similarity.item()
